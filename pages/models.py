72f148aba38d004303fd387d501fd1e416973f29
Andy Bayer <andrewbayer2016@u.northwestern.edu>
2015-02-04 14:19:03 -0600

    migrate new email model

--- pages/models.py
+++ pages/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Email(models.Model):
 	address = models.CharField(max_length=100, null=False)
-	site = models.CharField(max_length=10, null=False)
+	site = models.CharField(max_length=10, null=False, default='')
+
 	def __unicode__(self):
 		return self.address
