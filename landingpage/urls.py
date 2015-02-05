76d35ae69069f40a54fbeebf64fea5ba40d64129
Andy Bayer <andrewbayer2016@u.northwestern.edu>
2015-02-05 10:20:52 -0600

    fix statics

--- landingpage/urls.py
+++ landingpage/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 import pages.urls
+import settings
 
 urlpatterns = patterns('',
     # Examples:
@@ -10,3 +11,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include(pages.urls))
 )
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
